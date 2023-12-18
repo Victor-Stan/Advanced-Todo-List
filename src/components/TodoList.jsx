7bcdd675d873d10bb4923893f548a1ccc851f118
Victor Stanley <victorstanley2222@gmail.com>
2023-12-18 15:54:42 +0100

    new updates

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,25 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import { auth, db } from '../firebase-config';
-import { addDoc, collection, doc, deleteDoc, onSnapshot, setDoc } from 'firebase/firestore';
+import { addDoc, collection, query, where, doc, deleteDoc, onSnapshot, setDoc } from 'firebase/firestore';
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [task, setTask] = useState('');
   const [editTask, setEditTask] = useState({ id: null, task: '' });
+  const [statusFilter, setStatusFilter] = useState(null);
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(collection(db, 'todos'), (snapshot) => {
-      const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-      setTodos(data);
-    });
+    const fetchData = async () => { 
+      const user = auth.currentUser;
 
-    return () => unsubscribe();
-  }, [db]);
+      if (user) {
+        const q = query(
+          collection(db, 'todos'),
+          where('userId', '==', user.uid),
+          statusFilter ? where('status', '==', statusFilter) : null
+        );
+
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+          const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+          setTodos(data);
+        });
+
+        return () => unsubscribe();
+      }
+    };
+
+    fetchData();
+  }, [db, statusFilter]);
 
   const addTodo = async () => {
     await addDoc(collection(db, 'todos'), {
       userId: auth.currentUser.uid,
       task,
+      status: 'In Progress', 
     });
     setTask('');
   };
@@ -41,8 +57,22 @@ const TodoList = () => {
     setEditTask({ id: null, task: '' });
   };
 
+  const changeStatus = async (id, newStatus) => {
+    await setDoc(doc(db, 'todos', id), { status: newStatus }, { merge: true });
+  };
+
+  const handleStatusChange = async (id, currentStatus) => {
+    const newStatus = currentStatus === 'In Progress' ? 'Done' : 'In Progress';
+    await changeStatus(id, newStatus);
+  };
+
   return (
     <div>
+      <div>
+        <button onClick={() => setStatusFilter('Done')}>Done</button>
+        <button onClick={() => setStatusFilter('In Progress')}>In Progress</button>
+        
+      </div>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
@@ -60,6 +90,9 @@ const TodoList = () => {
                 {todo.task}
                 <button onClick={() => deleteTodo(todo.id)}>Delete</button>
                 <button onClick={() => startEdit(todo.id, todo.task)}>Edit</button>
+                <button onClick={() => handleStatusChange(todo.id, todo.status)}>
+                  {todo.status === 'In Progress' ? 'Done' : 'In Progress'}
+                </button>
               </>
             )}
           </li>
